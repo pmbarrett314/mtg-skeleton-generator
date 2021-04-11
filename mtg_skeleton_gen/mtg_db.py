9dfd9fc01a1195019baf16b12cbf3749a58a825d
Paul Barrett <pmbarrett314@gmail.com>
2021-04-10 22:31:53 -0500

    added pickling of specific sets

--- mtg_skeleton_gen/mtg_db.py
+++ mtg_skeleton_gen/mtg_db.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 
 from contextlib import closing
 
@@ -13,6 +14,29 @@ def card_manager(filename):
     return closing(MtgDB(filename))
 
 
-def update_db(filename):
+def update_db(filename=None):
+    if filename is None:
+        filename = db_file
+
     with card_manager(filename) as mtg_db:
         mtg_db.scryfall_update()
+
+
+def write_set_data(set_code, input_filename=None, output_filename=None):
+    if input_filename is None:
+        input_filename = db_file
+    if output_filename is None:
+        output_file = os.path.join(sets_dir, "{}.pickle".format(set_code))
+
+    with card_manager(input_filename) as mtg_db:
+        scryfall_cards = mtg_db.root.scryfall_cards
+        set_cards = list(scryfall_cards.where_exactly(set=set_code))
+        with open(output_file, "wb") as f:
+            pickle.dump(list(set_cards), f)
+
+
+def read_set_data(set_code, set_filename=None):
+    if set_filename is None:
+        set_filename = os.path.join(sets_dir, "{}.pickle".format(set_code))
+    with open(set_filename, "rb") as f:
+        return pickle.load(f)
